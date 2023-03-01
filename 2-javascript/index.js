cbad9387bf7f3fb45b2e1cfdc9c4c13373eae8c0
humanscape-jake <jake@humanscape.io>
2023-03-01 20:57:43 +0900

    docs: 브라우저 별 엔진정보등 정보 추가

--- 2-javascript/index.js
+++ 2-javascript/index.js
@@ -5,4 +5,16 @@ javascript engine
 다른 프로그래밍 언어 ex) c
 컴파일러: 실행하기전 모든 코드를 컴파일링
 
+
+브라우저 별 엔진
+ie: chakara
+chrome: v8
+safari: javascript core
+firefox: spider monkey
+
+문법의 규격사항, 표준사항
+ECMAScript: 브라우저 별 다른 엔진을 사용하기 때문에 javascript 에 대해 규격이 필요함
+
+앱서버 엔진
+node: v8
 */
