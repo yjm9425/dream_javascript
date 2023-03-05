03928090bcbd821f84c4e5e7ffac4c4ff45df7cf
Jungmin Yeom <yjm9425@gmail.com>
2023-03-05 22:42:29 +0900

    feat: boolean 타입 기본 예제

---
+++ 4-variables/boolean.js
@@ -0,0 +1,14 @@
+//Falsy
+console.log(0);
+console.log(-0);
+console.log(null);
+console.log(false);
+console.log(undefined);
+console.log(NaN);
+console.log("");
+
+// Truthy
+console.log([]);
+console.log({});
+console.log(true);
+console.log("false");
